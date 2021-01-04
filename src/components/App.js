3fddd5903ca69c2309ce6a25eefb8ca7e21188d7
Trex1888 <harpua123@gmail.com>
2021-01-04 01:26:26 -0600

    2nd commit

--- src/components/App.js
+++ src/components/App.js
@@ -33,39 +33,3 @@ function App() {
 }
 
 export default App;
-
-// import React from "react";
-// import "../styles/App.css";
-// import Chat from "../components/Chat";
-// import Sidebar from "../components/Sidebar";
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// import Login from "../components/Login";
-// import { useStateValue } from "../StateProvider";
-
-// function App() {
-//   const [{ user }, dispatch] = useStateValue();
-
-//   return (
-//     <div className="app">
-//       {!user ? (
-//         <Login />
-//       ) : (
-//         <div className="app-body">
-//           <Router>
-//             <Sidebar />
-//             <Switch>
-//               <Route path="/rooms/:roomId">
-//                 <Chat />
-//               </Route>
-//               <Route path="/">
-//                 <Chat />
-//               </Route>
-//             </Switch>
-//           </Router>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default App;
